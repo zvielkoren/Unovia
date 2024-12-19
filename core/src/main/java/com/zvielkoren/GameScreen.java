8a0b64172686ba365dafac035498cc233b1e25c7
zviel <zvielkoren@gmail.com>
2024-12-19 23:01:48 +0200

    Initial commit

---
+++ core/src/main/java/com/zvielkoren/GameScreen.java
@@ -0,0 +1,45 @@
+package com.zvielkoren;
+
+import com.badlogic.gdx.Screen;
+
+public class GameScreen implements Screen {
+    private final Main game;
+
+    public GameScreen(Main game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
